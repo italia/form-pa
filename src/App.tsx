8419117252be523c556d98d69b78ae2a75adac05
Alessandro Sebastiani <sebbalex@gmail.com>
2020-05-19 15:15:55 +0200

    added design-react-kit alongside with bootstrap-italia and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { JsonFormsDispatch } from '@jsonforms/react';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { JsonFormsDispatch } from "@jsonforms/react";
 
+import { Button } from "design-react-kit";
+import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
+import "typeface-titillium-web";
+import "typeface-roboto-mono";
+import "typeface-lora";
 
 function App() {
   return (
@@ -13,6 +18,12 @@ function App() {
       <div className="container">
         {/* other markup... */}
         <JsonFormsDispatch />
+        <Button color="primary" icon={false} tag="button">
+          Save
+        </Button>
+        <Button color="secondary" icon={false} tag="button">
+          Reset
+        </Button>
       </div>
     </div>
   );
