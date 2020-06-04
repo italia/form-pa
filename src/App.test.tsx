d2de94e33a430df119ee26f76eb8e7e8e675e298
Alessandro Sebastiani <sebbalex@gmail.com>
2020-06-04 17:35:32 +0200

    adding simple rendering tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { App } from "./App";
 
-it("renders welcome message", () => {
-  const { getByText } = render(<App />);
-  expect(getByText("Ente appartenenza/Owner")).toBeInTheDocument();
+
+describe("render components", ()=> {
+  it("renders welcome message", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Ente appartenenza/Owner")).toBeInTheDocument();
+  });
+
+  it("renders login button", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Accedi all'area personale")).toBeInTheDocument();
+  });
+
+  it("renders form buttons", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Save")).toBeInTheDocument();
+    expect(getByText("Reset")).toBeInTheDocument();
+  });
 });
