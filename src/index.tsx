682ade7f61672c0f72497eaf37f1c51e3405a76a
Alessandro Sebastiani <sebbalex@gmail.com>
2020-08-03 18:53:22 +0200

    feat: yaml and json support

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,46 @@ import { combineReducers, createStore } from "redux";
 import { Provider } from "react-redux";
 
 import { Actions, jsonformsReducer } from "@jsonforms/core";
-import schema from "./schema/schema.json";
-import uischema from "./schema/uischema.json";
 import {
   materialRenderers,
   materialCells,
 } from "@jsonforms/material-renderers";
 import { JsonFormsReduxContext } from "@jsonforms/react";
+import yaml from "js-yaml";
+const $RefParser = require("@apidevtools/json-schema-ref-parser");
+
+const isYAML = process.env.YAML_SOURCE || true;
+
+let schemaURL: string = "",
+  uischemaURL: string = "";
+
+if (isYAML) {
+  schemaURL = "schema/schema.yaml";
+  uischemaURL = "schema/uischema.yaml";
+} else {
+  schemaURL = "schema/schema.json";
+  uischemaURL = "schema/uischema.json";
+}
+
+const fetchSchema = async (url: string, dereference: boolean = false) => {
+  const text = await (await fetch(url)).text();
+  let out: string | object | undefined = text;
+  if (isYAML) {
+    out = yaml.safeLoad(text);
+  } else {
+    out = JSON.parse(text);
+  }
+  console.debug(url, out);
+
+  if (!dereference) return out;
+
+  try {
+    return await $RefParser.dereference(out);
+  } catch (err) {
+    console.error("Cannot dereference", err);
+    throw err;
+  }
+};
 
 // Setup Redux store
 const data = {
@@ -35,7 +68,21 @@ const store = createStore(combineReducers({ jsonforms: jsonformsReducer() }), {
   },
 });
 
-store.dispatch(Actions.init(data, schema, uischema));
+fetchSchema(schemaURL).then((schemaRetrieved) => {
+  console.log("schemaRetrieved", schemaRetrieved);
+
+  $RefParser.dereference(schemaRetrieved, (err: any, schema: any) => {
+    console.log("schema", err);
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    fetchSchema(uischemaURL, true).then((uischema) => {
+      const dataC = uischema._meta?.data || data || {};
+      store.dispatch(Actions.init(dataC, schema, uischema));
+    });
+  });
+});
 
 ReactDOM.render(
   <React.StrictMode>
