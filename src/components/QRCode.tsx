ce61dccb289ed1e61008b7af739d78238a7a268f
Alessandro Sebastiani <sebbalex@gmail.com>
2020-09-30 18:31:27 +0200

    feat: qrcode in modal window

--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import Qr from "qrcode.react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "design-react-kit";
 
-const qrCode = ({ data, display }: any) => {
+const qrCode = ({ data, display, toggle }: any) => {
   const string = JSON.stringify(data);
   // size will be minimum 128, maximum 512
   const size =
@@ -13,7 +20,21 @@ const qrCode = ({ data, display }: any) => {
       ? 512
       : (string.length / 100) * 128;
 
-  return <Qr value={string} size={size} style={{ display: display }} />;
+  return (
+    <Modal isOpen={display == "block" ? true : false} toggle={toggle}>
+      <ModalHeader charCode={215} closeAriaLabel="Close" tag="h5" wrapTag="div">
+        QRCode
+      </ModalHeader>
+      <ModalBody tag="div">
+        <Qr value={string} size={size} style={{ display: display }} />
+      </ModalBody>
+      <ModalFooter tag="div">
+        <Button color="secondary" icon={false} tag="button" onClick={toggle}>
+          Close
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
 };
 
 qrCode.propTypes = {
