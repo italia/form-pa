d27fd0cdf399507789ae185eb4f3152bb0ba5e93
Alessandro Sebastiani <sebbalex@gmail.com>
2020-11-03 22:46:01 +0100

    feat: pdf export with active fields - now pdf has active fields - export is dynamic following the dom schema - data are directly extracted from dom

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import { JsonFormsDispatch } from "@jsonforms/react";
 // @ts-ignore
 import { Button, Container } from "design-react-kit";
 import QRCode from "./components/QRCode";
+import { useStore } from "react-redux";
 
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 import "typeface-titillium-web/index.css";
 import "typeface-roboto-mono/index.css";
 import "typeface-lora/index.css";
 import { PDFDocument } from "pdf-lib";
+import { AnyAction, Store } from "redux";
 
 const download = (arrayBuffer: any, type: string) => {
   var blob = new Blob([arrayBuffer], { type: type });
@@ -24,7 +26,30 @@ const getDomElByJSONSchemaField = (elID: string): string => {
   return (document.getElementById(elID) as HTMLInputElement).value;
 };
 
-const getPDF = async (formRef: any) => {
+const createTextElement = (
+  page: any,
+  form: any,
+  label: string,
+  id: string,
+  w: number,
+  h: number
+) => {
+  console.log(label, id);
+  if (id === null || id === "") {
+    return;
+  }
+  page.drawText(label, {
+    x: 15,
+    y: h,
+    size: 12,
+  });
+
+  const nameField = form.createTextField(id);
+  nameField.setText(getDomElByJSONSchemaField(id));
+  nameField.addToPage(page, { x: 250, y: h, width: 250, height: 20 });
+};
+
+const getPDF = async (store: Store<any, AnyAction>) => {
   const pdfDoc = await PDFDocument.create();
 
   // Add a blank page to the document
@@ -35,27 +60,54 @@ const getPDF = async (formRef: any) => {
 
   const { width, height } = page.getSize();
 
-  //get static element
-  const nameFieldID = "#/properties/richiedente/properties/family_name2-input";
-  const nameLabel = "Name";
+  const schema = store.getState().jsonforms?.core?.schema?.properties;
+  if (schema === null) {
+    console.error("schema not defined", store.getState());
+  }
+  console.log("schema", store.getState().jsonforms);
 
-  const nameField = form.createTextField(nameLabel);
-  nameField.setText(getDomElByJSONSchemaField(nameFieldID));
-  nameField.addToPage(page, { x: 55, y: 640 });
+  const containerInnerHTML = document.querySelector("#container");
+  console.log("html", containerInnerHTML);
+
+  const allInputs = document.getElementsByTagName("input");
+
+  for (let index = 0; index < allInputs.length; ++index) {
+    let name = "",
+      el;
+    try {
+      el = document.querySelector(
+        `[for="${allInputs[index].id}"]`
+      ) as HTMLDivElement;
+    } catch {
+      el = null;
+    }
+    if (el === null) {
+      name = "id";
+    } else {
+      name = el.innerHTML;
+    }
+    createTextElement(
+      page,
+      form,
+      name,
+      allInputs[index].id,
+      width,
+      height - 70 * index
+    );
+  }
 
   const pdfBytes = await pdfDoc.save();
   download(pdfBytes, "application/pdf");
-  // const containerInnerHTML = document.querySelector("#ciao")!.innerHTML;
-  // const ref = ReactDOMServer.renderToString(containerInnerHTML);
-  // console.log(containerInnerHTML);
 };
 
 export const App = () => {
   const [isVisible, setVisible] = useState(false);
+  const store = useStore();
+
   return (
     <div className="App">
       <HeaderBar className="App-header" />
-      <Container>
+      <Container id={"container"}>
         <JsonFormsDispatch />
         <QRCode display={isVisible} toggle={() => setVisible(!isVisible)} />
         <Button color="primary" icon={false} tag="button">
@@ -72,7 +124,12 @@ export const App = () => {
         >
           Show/Hide QRCode
         </Button>{" "}
-        <Button color="secondary" icon={false} tag="button" onClick={getPDF}>
+        <Button
+          color="secondary"
+          icon={false}
+          tag="button"
+          onClick={() => getPDF(store)}
+        >
           Get PDF
         </Button>{" "}
       </Container>
