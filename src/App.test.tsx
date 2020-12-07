ad6c294118819c9af612523a6bfe19c9ab4d71ec
emilioSp <emilio.spatola@gmail.com>
2020-12-07 18:58:32 +0100

    replace cra with webpack

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -19,6 +19,7 @@
 import React from "react";
 // import { render } from "@testing-library/react";
 import { App } from "./App";
+// @ts-ignore
 import { render, fireEvent, screen } from '../test-utils';
 
 
