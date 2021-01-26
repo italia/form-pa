8c9369eb8b5469307f928b3d069f1a6127916d0d
Alessandro Sebastiani <sebbalex@gmail.com>
2021-01-26 15:25:40 +0100

    feat: unit tests for components

---
+++ src/Form.test.tsx
@@ -0,0 +1,75 @@
+/* form-pa: Send forms to PAs with SPID
+ * Copyright (C) 2020 Dipartimento per la Trasformazione Digitale
+ *                    Presidenza del Consiglio dei Ministri
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+// import { render } from "@testing-library/react";
+import { cleanup, screen, waitFor } from "@testing-library/react";
+import { Generate, JsonSchema7 } from "@jsonforms/core";
+import {
+  materialCells,
+  materialRenderers,
+} from "@jsonforms/material-renderers";
+import { act } from "react-dom/test-utils";
+import { JsonForms } from "@jsonforms/react";
+import { render } from "../test-utils";
+import { Form } from "./components/Form";
+
+afterEach(cleanup);
+
+const schema: JsonSchema7 = {
+  properties: {
+    foo: {
+      minLength: 5,
+      type: "string",
+    },
+  },
+  required: ["foo"],
+  type: "object",
+};
+
+const uischema = Generate.uiSchema(schema);
+const formParam = {
+  cells: materialCells,
+  data: {},
+  renderers: materialRenderers,
+  schema,
+  uischema,
+};
+
+jest.mock("./hooks/useFormParams", () => ({
+  useFormParams: () => [formParam],
+}));
+
+describe("render components", () => {
+  it.skip("renders form", async () => {
+    const { getByText } = render(<Form />);
+    // const { getByText } = render(<JsonForms {...formParam} />);
+
+    expect(getByText(/Foo/)).toBeInTheDocument();
+  });
+  it.skip("as", async () => {
+    await act(async () => {
+      render(
+        <JsonForms data={{}} schema={schema} renderers={materialRenderers} />
+      );
+    });
+    await waitFor(() => {
+      expect(screen.getByText(/Foo/)).toBeInTheDocument();
+    });
+  });
+});
---
+++ src/components/Footer.test.tsx
@@ -0,0 +1,47 @@
+import {
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import React from "react";
+import { render } from "../../test-utils";
+import { Footer } from "./Footer";
+
+describe("render components", () => {
+  const toggleQRCode = "Show/Hide QRCode";
+  it("renders form buttons", () => {
+    const { getByText } = render(<Footer />);
+    expect(getByText("Save")).toBeInTheDocument();
+    expect(getByText("Reset")).toBeInTheDocument();
+    expect(getByText(toggleQRCode)).toBeInTheDocument();
+  });
+
+  it("toggle modal clicking on show QRCode", async () => {
+    const { getByText, queryByText } = render(<Footer />);
+    const QRCodeText = "QRCode";
+
+    // check if it is closed
+    expect(queryByText(QRCodeText)).not.toBeInTheDocument();
+    const btnQR = getByText(toggleQRCode);
+    // opening modal
+    fireEvent(
+      btnQR,
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    await waitFor(() => {
+      expect(queryByText(QRCodeText)).toBeInTheDocument();
+    });
+    // closing modal
+    fireEvent(
+      btnQR,
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    await waitForElementToBeRemoved(() => queryByText(QRCodeText));
+  });
+});
