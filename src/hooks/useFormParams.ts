96960ecd4fb4f18ba8617297eca9da4607400099
Alessandro Sebastiani <sebbalex@gmail.com>
2021-02-03 18:50:45 +0100

    fix: data retrieved by spid should be pushed in redux

--- src/hooks/useFormParams.ts
+++ src/hooks/useFormParams.ts
@@ -1,13 +1,18 @@
 import { JsonFormsInitStateProps } from "@jsonforms/react";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setFormData } from "../redux/actions";
 import { loadSchema } from "../schemaLoader";
 
 export const useFormParams = () => {
   const [formParam, setFormParam] = useState<JsonFormsInitStateProps>();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
-      setFormParam(await loadSchema());
+      const param = await loadSchema();
+      setFormParam(param);
+      dispatch(setFormData({ data: param?.data }));
     };
     void fetchData();
   }, []);
