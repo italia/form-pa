3afb462763262945a694f8be3dd0a1951b499eec
Alessandro Sebastiani <sebbalex@gmail.com>
2021-02-15 18:23:34 +0100

    feat: form print and export

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 import { Button } from "design-react-kit";
+import { useReactToPrint } from "react-to-print";
 import { useDispatch, useSelector } from "react-redux";
 import Types from "MyTypes";
 import { setFormData, submitFormData } from "../redux/actions";
 import QRCodeConnected from "./QRCodeConnected";
 import { ReviewModal } from "./ReviewModal";
 
-export const Footer = (): JSX.Element => {
+interface FooterProps {
+  readonly formRef: React.RefObject<HTMLDivElement>;
+}
+
+export const Footer = ({ formRef }: FooterProps): JSX.Element => {
   const [isQrModalVisible, setQrModalVisible] = useState(false);
   const [isReviewModalVisible, setReviewModalVisible] = useState(false);
   const jsonformsData = useSelector(
     (state: Types.RootState) => state.form?.data
   );
+
+  const handlePrint = useReactToPrint({
+    content: () => formRef.current,
+  });
 
   const dispatch = useDispatch();
   const handleSubmit = () => {
@@ -26,7 +35,7 @@ export const Footer = (): JSX.Element => {
     );
   };
   return (
-    <div className="pb-3">
+    <div className="pb-3 float-right">
       <QRCodeConnected
         display={isQrModalVisible}
         toggle={(): void => setQrModalVisible(!isQrModalVisible)}
@@ -36,6 +45,36 @@ export const Footer = (): JSX.Element => {
         toggle={(): void => setReviewModalVisible(!isReviewModalVisible)}
       />
       <Button
+        outline
+        color="primary"
+        icon={false}
+        tag="button"
+        onClick={clearData}
+        data-testid="reset-button"
+      >
+        Cancella tutto
+      </Button>{" "}
+      <Button
+        outline
+        color="primary"
+        icon={false}
+        tag="button"
+        data-testid="print-button"
+        onClick={handlePrint}
+      >
+        Stampa
+      </Button>{" "}
+      <Button
+        outline
+        color="primary"
+        icon={false}
+        tag="button"
+        onClick={(): void => setQrModalVisible(!isQrModalVisible)}
+        data-testid="qr-modal-button"
+      >
+        Scarica QR Code
+      </Button>{" "}
+      <Button
         color="primary"
         icon={false}
         tag="button"
@@ -43,24 +82,6 @@ export const Footer = (): JSX.Element => {
         data-testid="review-modal-button"
       >
         Save
-      </Button>{" "}
-      <Button
-        color="secondary"
-        icon={false}
-        tag="button"
-        onClick={clearData}
-        data-testid="reset-button"
-      >
-        Reset
-      </Button>{" "}
-      <Button
-        color="secondary"
-        icon={false}
-        tag="button"
-        onClick={(): void => setQrModalVisible(!isQrModalVisible)}
-        data-testid="qr-modal-button"
-      >
-        Show/Hide QRCode
       </Button>{" "}
     </div>
   );
